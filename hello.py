cac2a8950cb434e4d0ad21fd00228dadb567b3cb
Sri Harsha <sriharsha0603@gmail.com>
2026-05-27 15:18:53 +0530

    fresh day 1 setup

---
+++ hello.py
@@ -0,0 +1,14 @@
+import sys
+name = "AI Architect"
+skills=["Big Data","LLM","RAG","Python"]
+
+print (f"python Verison:{sys.version}")
+print(f"Name:{name}")
+print()
+
+for skill in skills:
+    print(f"Learning:{skill}")
+
+print()
+print("Day 1 complete. Week 1 begins.")
+
